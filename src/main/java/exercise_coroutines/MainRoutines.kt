7ce0480b549aff6ec073ed910ee614e7bdb05027
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-03-13 15:59:36 +0200

    Initial commit

---
+++ src/main/java/exercise_coroutines/MainRoutines.kt
@@ -0,0 +1,13 @@
+package exercise_coroutines
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+suspend fun main() = coroutineScope {
+    launch {
+        delay(1000)
+        println("Kotlin Coroutines World!")
+    }
+    println("Hello")
+}
