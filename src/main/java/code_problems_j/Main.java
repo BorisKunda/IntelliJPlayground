8be035b1d092c5037554178f874ff0312ddaa62e
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-03-19 21:06:54 +0200

    commit 5

--- src/main/java/code_problems_j/Main.java
+++ src/main/java/code_problems_j/Main.java
@@ -1,34 +1,11 @@
 package code_problems_j;
 
+
 public class Main {
 
     public static void main(String[] args) {
 
-        String txt = "ABC";
 
-        char[] charactersFromStringArr = txt.toCharArray();
-
-        for (int i = 0; i < charactersFromStringArr.length; i++) {
-
-            char charTemp = charactersFromStringArr[i];
-            int intFromChar = Character.valueOf(charTemp);
-            String stringFromChar = String.valueOf(charTemp);
-
-            if (intFromChar != 32) {//check if empty space
-                print(stringFromChar);
-            } else {
-                print("Fail");
-            }
-
-        }
-
-
-    }
-
-    public static String[] stringToArray(String s) {
-
-
-        return null;//new String[]{n..}
 
     }
 
@@ -44,4 +21,4 @@ public class Main {
 
 }
 
-//String#split().
+
