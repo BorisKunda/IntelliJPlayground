f7a51d196e9929d62f728743e4c3517be9123522
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-04-02 18:38:29 +0300

    commit 11

--- src/main/java/exercise_coroutines/MainRoutines.kt
+++ src/main/java/exercise_coroutines/MainRoutines.kt
@@ -1,48 +1,13 @@
 package exercise_coroutines
 
 import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 
 fun main() {
 
-    val c1: CoroutineContext = CoroutineName("corA")
-    //println(c1[CoroutineName])
-    val c2: CoroutineContext = CoroutineName("corB")
-    //println(c2[CoroutineName])
-    val c3: CoroutineContext = CoroutineName("corC")
-    //println(c3[CoroutineName])
-
-    GlobalScope.launch() {
+    GlobalScope.launch {
 
     }
-
-    //coroutine-scope -> inherited context
-    //launch(context) -> context argument
-    //launch(block) -> coroutine context
-
-    /*
-    fun CoroutineScope.newCoroutineContext(context: CoroutineContext): CoroutineContext {
-    val combined = coroutineContext + context
-
-    coroutineContext -> inheritedContext
-    context -> context argument
-
-    }
-
-    parent context = default values + inherited context + context argument
-     */
-
-    //coroutine context = parent context + coroutine job
-
-//    GlobalScope.launch(
-//        context = Dispatchers.Default,
-//        block = {
-//            ->
-//        },
-//    )
-
 
 }
 
