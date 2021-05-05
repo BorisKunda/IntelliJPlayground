995952c0810f502b794c54b689ac0da5bd0c683e
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-05 18:34:02 +0300

    matrix special value count

--- src/main/java/regularj/Main.java
+++ src/main/java/regularj/Main.java
@@ -7,64 +7,54 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int rows, columns, outerIndex, innerIndex, count = 0, specialValue;
-        Scanner scanner = new Scanner(System.in);
+        int arr1D[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
 
-        System.out.println("Enter the number of rows:");
-        rows = scanner.nextInt();
-        System.out.println("Enter the number of columns:");
-        columns = scanner.nextInt();
 
-        int matrix[][] = new int[rows][columns];
+        int[][] matrixArr2d = {
+                {1, 1},
+                {0, 0},
+        };
 
-        System.out.println("Enter the elements of the matrix:");
+
+        print2D(matrixArr2d);
+
+        System.out.println("---");
+
+        System.out.println("count: " + getCountOccurrences2D(1, matrixArr2d));
+
+    }
+
+    public static int getCountOccurrences2D(int specialVal, int[][] matrix) {
+
+        int rows, columns, outerIndex, innerIndex, count = 0;
+
+        rows = matrix.length;
+        columns = matrix[0].length;
 
         for (outerIndex = 0; outerIndex < rows; outerIndex++) {
             for (innerIndex = 0; innerIndex < columns; innerIndex++) {
-                matrix[outerIndex][innerIndex] = scanner.nextInt();
+
+                if (matrix[outerIndex][innerIndex] == specialVal) {
+                    count++;
+                }
+
             }
         }
 
-        print2D(matrix);
-
-        System.out.println("Enter the value for number x:");
-        specialValue = scanner.nextInt();
-
-        for (outerIndex = 0; outerIndex < rows; outerIndex++) {
-            if (outerIndex == specialValue)
-                count++;
-        }
-
-        for (innerIndex = 0; innerIndex < columns; innerIndex++) {
-            if (innerIndex == specialValue)
-                count++;
-        }
-
-        System.out.println("Number x appears " + count + " times.");
-
-        /**-OLD----------------------------------------------------------------------------------------------*/
-        /**-OLD----------------------------------------------------------------------------------------------*/
-        /**-OLD----------------------------------------------------------------------------------------------*/
-        /**-OLD----------------------------------------------------------------------------------------------*/
-
-        //Count occurrences of specific number scanner an array (1D)
-        int arr1D[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
-        int n = arr1D.length;
-        int x = 2;
-        System.out.println(countOccurrences1D(arr1D, n, x));
+        return count;
 
     }
 
-    public static void print2D(int mat[][]) {
-        // Loop through all rows
-        for (int[] row : mat)
-
-            // converting each row as string
-            // and then printing in a separate line
+    public static void print2D(int matrix[][]) {
+        for (int[] row : matrix)
             System.out.println(Arrays.toString(row));
     }
 
-    static int countOccurrences1D(int arr[], int n, int x) {
+    static int countOccurrences1D(int arr[]) {
+
+        int n = arr.length;
+        int x = 2;
+
         int res = 0;
         for (int i = 0; i < n; i++)
             if (x == arr[i])
