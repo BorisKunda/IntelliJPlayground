183b1ffc7a71e6b715e536f0f14d5701b2f966e9
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-06 01:00:46 +0300

    w

--- src/main/java/dfsalgorithm/Magic.java
+++ src/main/java/dfsalgorithm/Magic.java
@@ -1,6 +1,7 @@
 package dfsalgorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public class Magic {
 
     //loop [] integer --> int[]
 
-    static Integer[] magic(int[][] arr) {
+    private static Integer[] magic(int[][] arr) {
         List<Integer> res = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
@@ -22,7 +23,7 @@ public class Magic {
         return res.toArray(new Integer[0]);
     }
 
-    static int lemonSize(int i, int j, int[][] arr) {
+    private static int lemonSize(int i, int j, int[][] arr) {
         if (i >= 0 && i < arr.length && j >= 0 && j < arr[0].length) {
             if (arr[i][j] == 1) {
                 arr[i][j] = 0;
@@ -38,35 +39,23 @@ public class Magic {
     }
 
 
-    static void printResult(Integer[] result) {
-        for (Integer r : result) {
-            System.out.println(r);
-        }
-    }
-
     public static void main(String[] args) {
 
-        int[][] mat1 = {
+        int[][] matrix1 = {
                 {1, 0, 0, 1},
                 {0, 0, 1, 0},
                 {1, 0, 1, 0},
                 {1, 0, 1, 0}
         };
-        Integer[] result = magic(mat1);
-        System.out.println("Matrix1");
-        printResult(result);
 
-        int[][] mat2 = {
+        int[][] matrix2 = {
                 {0, 0, 0, 1, 0, 0, 0, 1},
                 {1, 1, 0, 0, 0, 0, 1, 0},
                 {0, 0, 0, 0, 1, 1, 0, 1},
                 {1, 1, 1, 0, 0, 0, 0, 0}
         };
-        result = magic(mat2);
-        System.out.println("Matrix2");
-        printResult(result);
 
-        int[][] mat3 = {
+        int[][] matrix3 = {
                 {0, 0, 0, 0, 1},
                 {0, 1, 1, 1, 0},
                 {0, 1, 0, 1, 0},
@@ -74,9 +63,12 @@ public class Magic {
                 {1, 0, 0, 0, 0}
         };
 
-        result = magic(mat3);
-        System.out.println("Matrix3");
-        printResult(result);
+        System.out.println("Matrix1" + Arrays.toString(magic(matrix1)));
+
+        System.out.println("Matrix2" + Arrays.toString(magic(matrix2)));
+
+        System.out.println("Matrix3" + Arrays.toString(magic(matrix3)));
+
     }
 
 }
