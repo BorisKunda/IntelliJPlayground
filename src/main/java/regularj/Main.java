1ce58c6c98fb1acac53e55fda32284aab9542cc2
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-16 22:32:26 +0300

    added params to template

--- src/main/java/regularj/Main.java
+++ src/main/java/regularj/Main.java
@@ -5,17 +5,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /**--Data--*/
+        /**--Variables--*/
 
-
-        /**--Methods--*/
 
     }
+
+    /**
+     * --Methods--
+     */
 
     public static void print(String s) {
         System.out.println(s);
     }
 
+    class SampleObj {
+
+        private String name;
+
+        public SampleObj(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 
 }
 
