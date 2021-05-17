ffb3c2504b959f28bc603b4cf91743ad9608dab6
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-17 12:18:52 +0300

    commit 33

--- src/main/java/regularj/Main.java
+++ src/main/java/regularj/Main.java
@@ -7,16 +7,22 @@ public class Main {
 
         /**--Variables--*/
 
-
-
     }
 
     /**
      * --Methods--
      */
 
+    /**
+     * --DON'T REMOVE--
+     */
+
+    public static void print() {
+        System.out.println("EMPTY MESSAGE");
+    }
+
     public static void print(String s) {
-        System.out.println(s);
+        System.out.println("MESSAGE: " + s);
     }
 
 }
