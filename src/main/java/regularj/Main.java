d94d7ac0f3286aa3f4eb3915095c172fe1e738c8
Boris Kunda <34305099+BorisKunda@users.noreply.github.com>
2021-05-17 10:08:21 +0300

    added params to template 2

--- src/main/java/regularj/Main.java
+++ src/main/java/regularj/Main.java
@@ -8,6 +8,7 @@ public class Main {
         /**--Variables--*/
 
 
+
     }
 
     /**
@@ -16,23 +17,6 @@ public class Main {
 
     public static void print(String s) {
         System.out.println(s);
-    }
-
-    class SampleObj {
-
-        private String name;
-
-        public SampleObj(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
     }
 
 }
---
+++ src/main/java/regularj/SampleObj.java
@@ -0,0 +1,18 @@
+package regularj;
+
+public class SampleObj {
+
+    private String name;
+
+    public SampleObj(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
